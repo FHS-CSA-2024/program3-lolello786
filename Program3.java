71970cad9b5c437733705be22793c7adb54ec37b
lolello786 <mr.zubro@gmail.com>
2024-08-13 09:45:56 -0700

    Update Program3.java
    i did program 3 and added a little code to detect if it is a rectangle or a square :)

--- Program3.java
+++ Program3.java
@@ -3,12 +3,46 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
+class MyClass{
+    public static void main(String[] args) {
+        double width;
+        double length;
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter Width: ");
+        width = myScanner.nextInt();
+        System.out.println("Enter Length: ");
+        length = myScanner.nextInt();
+        double area = width * length;
+        double peri = (width * 2) + (length * 2);
+        if(length == width)
+        {
+            System.out.println("Square Width is: " + width);
+        System.out.println("Square Length is: " + length);
+        System.out.println("Square Area is: " + area);
+        System.out.println("Square Perimiter is: " + peri);
+        }
+        else
+        {
+        System.out.println("Rectangle Width is: " + width);
+        System.out.println("Rectangle Length is: " + length);
+        System.out.println("Rectangle Area is: " + area);
+        System.out.println("Rectangle Perimiter is: " + peri);   
+    }
+    }
+}
 
 
 
 //Paste console output below:
 /*
+Rectangle Width is: 126.0
+Rectangle Length is: 6423.0
+Rectangle Area is: 809298.0
+Rectangle Perimiter is: 13098.0
 
+Square Width is: 156.0
+Square Length is: 156.0
+Square Area is: 24336.0
+Square Perimiter is: 624.0
 
 */
